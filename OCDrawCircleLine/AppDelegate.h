bfbceffe9c6c441812f3efd197653c3a0294c3b7
Takuya Aso <milanista224@gmail.com>
2016-02-29 21:14:16 +0900

    Initial Commit

---
+++ OCDrawCircleLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCDrawCircleLine
+//
+//  Created by 麻生 拓弥 on 2016/02/29.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
